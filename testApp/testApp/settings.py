7931ee7f72dd04ab459059c3623dbb38bc99690b
Damian Banach <damian070799@wp.pl>
2022-10-18 16:05:14 +0200

    config database

--- testApp/testApp/settings.py
+++ testApp/testApp/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'testApp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'aplikacjewww',
+        'USER': 'postgres',
+        'PASSWORD': 'damian',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
