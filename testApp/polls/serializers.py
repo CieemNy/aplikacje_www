9566b7fc02109600fb0325da865b041fd95dedd4
Damian Banach <damian070799@wp.pl>
2022-10-26 13:07:06 +0200

    created QuestionModelSerializer & ChoiceModelSerializer

--- testApp/polls/serializers.py
+++ testApp/polls/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Osoba, Druzyna, MIESIAC_URODZENIA
+from .models import Osoba, Druzyna, MIESIAC_URODZENIA, Question, Choice
 
 class OsobaSerializer(serializers.Serializer):
     id = serializers.IntegerField(read_only=True)
@@ -32,3 +32,17 @@ class DruzynaSerializer(serializers.Serializer):
         instance.nazwa = validated_data.get('nazwa', instance.nazwa)
         instance.save()
         return instance
+
+
+class QuestionModelSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Question
+        fields = ['id', 'question_text', 'pub_date']
+        read_only_fields = ['id']
+
+
+class ChoiceModelSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Choice
+        fields = ['id', 'question', 'choice_text', 'votes']
+        read_only_fields = ['id']
