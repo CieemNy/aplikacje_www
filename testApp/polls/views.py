3b149b3eed3b5e9953596fbf4a59ee80b562c8c9
Damian Banach <damian070799@wp.pl>
2022-11-06 14:54:27 +0100

    created OsobaList using APIView

--- testApp/polls/views.py
+++ testApp/polls/views.py
@@ -1,8 +1,23 @@
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework.views import APIView
 from .models import Osoba, Druzyna
 from .serializers import OsobaSerializer, DruzynaSerializer
+
+
+class OsobaList(APIView):
+    def get(self):
+        osoby = Osoba.objects.all()
+        serializer = OsobaSerializer(osoby, many=True)
+        return Response(serializer.data)
+    def post(self, request, format=None):
+        serializer = OsobaSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
 
 
 # @api_view(['GET', 'POST'])
