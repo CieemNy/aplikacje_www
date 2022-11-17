ed056ff832799f9b4977ca5aa98fa0247e47c654
Damian Banach <damian070799@wp.pl>
2022-11-17 17:43:42 +0100

    added path to new endpoints

--- testApp/polls/urls.py
+++ testApp/polls/urls.py
@@ -4,6 +4,8 @@ from . import views
 urlpatterns = [
     path('osoby/', views.osoba_list),
     path('osoby/<int:pk>/', views.osoba_detail),
+    path('osoby/update/<int:pk>/', views.osoba_edit),
+    path('osoby/delete/<int:pk>/', views.osoba_delete),
     path('osoby/<str:znak>/', views.osoba_znak),
     path('druzyny/', views.druzyna_list),
     path('druzyny/<int:pk>/', views.druzyna_detail),
