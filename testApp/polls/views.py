73cc43440c7bd623e0d7eaca82d44173bc9a3bde
Damian Banach <damian070799@wp.pl>
2022-11-17 17:39:58 +0100

    added imports

--- testApp/polls/views.py
+++ testApp/polls/views.py
@@ -1,5 +1,8 @@
+from django.shortcuts import render
 from rest_framework import status
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, authentication_classes, permission_classes
+from rest_framework.authentication import SessionAuthentication, BasicAuthentication
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from .models import Osoba, Druzyna
 from .serializers import OsobaSerializer, DruzynaSerializer
