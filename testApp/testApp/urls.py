70bdf1078c63e7cc38d1bd05efc3fc16b477d008
Damian Banach <damian070799@wp.pl>
2022-11-17 17:44:21 +0100

    added path to api-auth/

--- testApp/testApp/urls.py
+++ testApp/testApp/urls.py
@@ -5,5 +5,6 @@ from rest_framework.authtoken.views import obtain_auth_token
 urlpatterns = [
     path('polls/', include('polls.urls')),
     path('admin/', admin.site.urls),
+    path('api-auth/', include('rest_framework.urls')),
     path('login/', obtain_auth_token, name="obtain-auth-token"),
 ]
